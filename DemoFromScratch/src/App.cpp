6b901ed17a5dc75f967b666ad4e59b745882ed7c
Pat Lillis <lillispm@gmail.com>
2024-01-02 15:42:43 -0700

    Matrix examples

--- DemoFromScratch/src/App.cpp
+++ DemoFromScratch/src/App.cpp
@@ -15,11 +15,22 @@ using namespace std;
 using namespace DirectX;
 using namespace DirectX::PackedVector;
 
-ostream& XM_CALLCONV operator<<(ostream& os, FXMVECTOR v) {
+ostream& XM_CALLCONV operator <<(ostream& os, FXMVECTOR v) {
 	XMFLOAT3 dest;
 	XMStoreFloat3(&dest, v);
 
 	os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ")";
+	return os;
+}
+
+ostream& XM_CALLCONV operator << (ostream& os, FXMMATRIX m) {
+	for (int i = 0; i < 4; i++) {
+		os << XMVectorGetX(m.r[i]) << "\t";
+		os << XMVectorGetY(m.r[i]) << "\t";
+		os << XMVectorGetZ(m.r[i]) << "\t";
+		os << XMVectorGetW(m.r[i]);
+		os << endl;
+	}
 	return os;
 }
 
@@ -32,46 +43,31 @@ int main()
 		return 1;
 	}
 
-	XMVECTOR n = XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
-	XMVECTOR u = XMVectorSet(1.0f, 2.0f, 3.0f, 0.0f);
-	XMVECTOR v = XMVectorSet(-2.0f, 1.0f, -3.0f, 0.0f);
-	XMVECTOR w = XMVectorSet(0.707f, 0.707f, 0.0f, 0.0f);
+	XMMATRIX A(
+		1.0f, 0.0f, 0.0f, 0.0f,
+		0.0f, 2.0f, 0.0f, 0.0f,
+		0.0f, 0.0f, 4.0f, 0.0f,
+		1.0f, 2.0f, 3.0f, 1.0f
+	);
 
-	XMVECTOR a = u + v;
-	XMVECTOR b = u - v;
-	XMVECTOR c = 10.0f * u;
-	XMVECTOR L = XMVector3Length(u);
-	XMVECTOR d = XMVector3Normalize(u);
-	XMVECTOR s = XMVector3Dot(u, v);
-	XMVECTOR e = XMVector3Cross(u, v);
+	XMMATRIX B = XMMatrixIdentity();
 
-	XMVECTOR projW;
-	XMVECTOR perpW;
-	XMVector3ComponentsFromNormal(&projW, &perpW, w, n);
+	XMMATRIX C = A * B;
 
-	bool equal = XMVector3Equal(projW + perpW, w);
-	bool notEqual = XMVector3NotEqual(projW + perpW, w);
+	XMMATRIX D = XMMatrixTranspose(A);
 
-	XMVECTOR angleVec = XMVector3AngleBetweenVectors(projW, perpW);
-	float angleRadians = XMVectorGetX(angleVec);
-	float angleDegrees = XMConvertToDegrees(angleRadians);
+	XMVECTOR det = XMMatrixDeterminant(A);
+	XMMATRIX E = XMMatrixInverse(&det, A);
 
-	cout << "u                  = " << u << endl;
-	cout << "v                  = " << v << endl;
-	cout << "w                  = " << w << endl;
-	cout << "n                  = " << n << endl;
-	cout << "a = u + v          = " << a << endl;
-	cout << "b = u - v          = " << b << endl;
-	cout << "c = 10 * u         = " << c << endl;
-	cout << "d = u / ||u||      = " << d << endl;
-	cout << "e = u x v          = " << e << endl;
-	cout << "L = ||u||          = " << L << endl;
-	cout << "s = u.v            = " << s << endl;
-	cout << "projW              = " << projW << endl;
-	cout << "perpW              = " << perpW << endl;
-	cout << "projW + perpW == w = " << equal << endl;
-	cout << "projW + perpW != w = " << notEqual << endl;
-	cout << "angle              = " << angleDegrees << endl;
+	XMMATRIX F = A * E;
+
+	cout << "A = " << endl << A << endl;
+	cout << "B = " << endl << B << endl;
+	cout << "C = A*B = " << endl << C << endl;
+	cout << "D = transpose(A) = " << endl << D << endl;
+	cout << "det = determinant(A) = " << det << endl << endl;
+	cout << "E = inverse(A) = " << endl << E << endl;
+	cout << "F = A*E = " << endl << F << endl;
 
 	return 0;
 }
